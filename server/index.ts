5fd9327b9d561a99ee7006f6930d295c7e41e7c9
Kavita Bisht <130052746+Kavita0011@users.noreply.github.com>
2025-06-21 20:00:42 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
-    const status = err.status || err.statusCode || 500;
+    const status = err.status || err.statusCode || 10000;
     const message = err.message || "Internal Server Error";
 
     res.status(status).json({ message });
